03241b9d4567ed15e231ff1779be4432d5f48398
kelvin-wu13 <159217927+kelvin-wu13@users.noreply.github.com>
2025-08-07 20:51:01 +0700

    SimpleMapDone

--- Assets/Script/HouseController.cs
+++ Assets/Script/HouseController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HouseController : MonoBehaviour
 {
@@ -6,18 +7,53 @@ public class HouseController : MonoBehaviour
 
     public Sprite highlightSprite;
     public Sprite lockedSprite;
+    public Sprite starCompleteSprite;
+    public Sprite starIncompleteSprite;
+
     public GameObject starsDisplayContainer;
 
     private Sprite defaultSprite;
     private SpriteRenderer spriteRenderer;
     private bool isUnlocked = false;
 
-    private void Start()
+    private Camera mainCamera;
+    private bool isHovering = false;
+
+    private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         defaultSprite = spriteRenderer.sprite;
+        mainCamera = Camera.main;
+    }
+    private void Start()
+    {
         UpdateVisualState();
     }
+    private void Update()
+    {
+        if (!isUnlocked) return;
+
+        RaycastHit2D hit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Pointer.current.position.ReadValue()));
+      
+        bool mouseIsOver = (hit.collider != null && hit.collider.gameObject == this.gameObject);
+
+        if (mouseIsOver && !isHovering)
+        {
+            isHovering = true;
+            spriteRenderer.sprite = highlightSprite;
+        }
+        else if (!mouseIsOver && isHovering)
+        {
+            isHovering = false;
+            spriteRenderer.sprite = defaultSprite;
+        }
+
+        if (isHovering && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            MapManager.Instance.ShowLevelPopup(this);
+        }
+    }
+
 
     public void UpdateVisualState()
     {
@@ -39,11 +75,25 @@ public class HouseController : MonoBehaviour
     private void UpdateStarsOnMap()
     {
         if (starsDisplayContainer == null) return;
+
         starsDisplayContainer.SetActive(true);
-        int starCount = GameProgress.GetStars(levelData.levelIndex);
+        int completedStars = GameProgress.GetStars(levelData.levelIndex);
+
         for (int i = 0; i < starsDisplayContainer.transform.childCount; i++)
         {
-            starsDisplayContainer.transform.GetChild(i).gameObject.SetActive(i < starCount);
+            SpriteRenderer starRenderer = starsDisplayContainer.transform.GetChild(i).GetComponent<SpriteRenderer>();
+            if (starRenderer == null) continue;
+
+            starRenderer.gameObject.SetActive(true);
+
+            if (i < completedStars)
+            {
+                starRenderer.sprite = starCompleteSprite;
+            }
+            else
+            {
+                starRenderer.sprite = starIncompleteSprite;
+            }
         }
     }
 
@@ -51,7 +101,10 @@ public class HouseController : MonoBehaviour
     {
         if (isUnlocked)
         {
-            spriteRenderer.sprite = highlightSprite;
+            if (spriteRenderer.sprite == defaultSprite)
+            {
+                spriteRenderer.sprite = highlightSprite;
+            }
         }
     }
     private void OnMouseExit()
